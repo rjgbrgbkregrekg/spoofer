e4c96603422f87c9484c4846fc6ad45610273d71
creedalameen2 <creedalameen@gmail.com>
2025-04-14 11:54:45 -0700

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+
+set "currentDir=%~dp0"
+"%currentDir%Nebel.exe" /SU auto
+"%currentDir%Nebel.exe" /BS %RANDOM%-%RANDOM%-%RANDOM%
+"%currentDir%Nebel.exe" /CS %RANDOM%-%RANDOM%-%RANDOM%
+
+pause
